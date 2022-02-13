214deb57c0c7f9a2c149d7d3fd6fd5c0ceab3706
anamerasul <anamerasul@gmail.com>
2022-02-13 17:47:35 +0600

    shopping cart handle decrease and update price

--- app.js
+++ app.js
@@ -1,29 +1,81 @@
-document.getElementById('case-plus-1').addEventListener('click', function(){
-        console.log('plus clicked');
-
-        const caseInput=document.getElementById('input-value-1');
+function updateCaseNumber(inputId,isIncrease,anotherId, price){
+        const caseInput=document.getElementById(inputId);
         const caseNumberText=caseInput.value;
-        const  caseNumber=parseInt(caseNumberText);
+        let caseNumber=parseInt(caseNumberText);
+        if(isIncrease===true){
+                caseNumber=caseNumber+1;
+}
 
-        caseInput.value=caseNumber+1;
-        console.log(caseNumber)
+else if(caseNumber>0){
+        
+        caseNumber=caseNumber-1;
 
+}
+
+else{
+        return false;
+}
+
+caseInput.value=caseNumber
+
+const caseTotal =document.getElementById(anotherId);
+caseTotal.innerText=caseNumber*price
+}
+
+
+
+document.getElementById('phone-plus').addEventListener('click', function(){
+        // console.log('plus clicked');
+
+        const price=1219
+        updateCaseNumber('phone-input-value',true,'phone-total',price)
 
 });
 
-document.getElementById('case-minus-1').addEventListener('click', function(){
-        console.log('minus clicked');
+document.getElementById('phone-minus').addEventListener('click', function(){
+        // console.log('minus clicked');
+        const price=1219
+        // const caseInput=document.getElementById('input-value-1');
+        // const caseNumberText=caseInput.value;
+        // const  caseNumber=parseInt(caseNumberText);
 
-        const caseInput=document.getElementById('input-value-1');
-        const caseNumberText=caseInput.value;
-        const  caseNumber=parseInt(caseNumberText);
+        // if(caseNumber<=0){
+        //         return false;
+        // }
 
-        if(caseNumber<=0){
-                return false;
-        }
+        // caseInput.value=caseNumber-1;
+        // console.log(caseNumber)
 
-        caseInput.value=caseNumber-1;
-        console.log(caseNumber)
+        updateCaseNumber('phone-input-value',false,'phone-total',price)
 
+
+})
+document.getElementById('case-plus').addEventListener('click', function(){
+        // console.log('plus clicked');
+        const price=59;
+        // const caseInput=document.getElementById('input-value-2');
+        // const caseNumberText=caseInput.value;
+        // const  caseNumber=parseInt(caseNumberText);
+
+        // caseInput.value=caseNumber+1;
+        // console.log(caseNumber)
+        updateCaseNumber('case-input-value',true,'case-total',price);
+
+});
+
+document.getElementById('case-minus').addEventListener('click', function(){
+        // console.log('minus clicked');
+        const price=59;
+        // const caseInput=document.getElementById('input-value-2');
+        // const caseNumberText=caseInput.value;
+        // const  caseNumber=parseInt(caseNumberText);
+
+        // if(caseNumber<=0){
+        //         return false;
+        // }
+
+        // caseInput.value=caseNumber-1;
+        // console.log(caseNumber)
+        updateCaseNumber('case-input-value',false,'case-total',price);
 
 })
